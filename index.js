1c3075a0708be25ba19e16393848f6771f8047db
nazaninjh <nazanin.jh97@gmail.com>
2023-08-12 13:39:45 +0330

    Add changes to index.js
    removed the computer player and added a real player because the logic
    was not working. players can add their marks on the screen now and play
    their turns. future update is needed to declare the winner and end the
    game.

--- index.js
+++ index.js
@@ -1,83 +1,76 @@
-//using an IIFE module because gameBoard is only needed once.
-const GameBoard = (function () {
-  const gameBoard = ["X", "O"];
-  var box;
-  const boxArray = [];
-  const boxCont = document.querySelector(".item-cont");
-  const createBox = ()=> {
-    
-    for (let i=0; i < 9; i++) {
-        box = document.createElement("div");
-        box.setAttribute("class", `item ${i}`);
-        boxCont.appendChild(box);
-        // use an array to store the boxes
-        boxArray.push(box);
-        // because box variable is always getting updated till item 8
-           
-    }
-    
-    
-  }
-   
-  const catchClick = ()=> {
-    boxArray.forEach((item)=> item.addEventListener("click", getUserPosition));
-    function getUserPosition() {
-      if (this.textContent == "") {
-        this.textContent = "X";
-      };
-      giveRandNum();
-      
-    }
-  }
-  const giveRandNum = ()=> {
-    let answer =  Math.floor(Math.random() * 9);
-    console.log(answer)
-    getComputerPosition();
-    function getComputerPosition() {
-      boxArray.forEach((item) => {
-        // problem: can not replace an already taken box.
-        if (item.textContent != "") {
-          console.log('full');
-          
-        } else if (boxArray.indexOf(item) === answer && item.textContent ===  "") {
-          boxArray[answer].textContent = "O"; 
-        }  ;
-      });
-      return;
-    };
-  };
-  
-  
-  return {
-    giveRandNum,
-    createBox,
-    catchClick
-  };
-}());
-GameBoard.createBox();
-console.log(GameBoard.giveRandNum());
-GameBoard.catchClick();
-
+// avoid repetition 
+// use as little public functions and variables as you can
+// try ti save memory and enhance performance
+var flag;
+const boxArray = [];
 // to save memory, use prototype
 // create a base 
-const projectProto = () => {
+const projectProto = { 
   
 };
 // using a factory function because multiple objects are needed to be created.
 
 const Players = (name, mark) => {
+    return Object.create(projectProto, {
+      name: {
+        value:name
+      }, 
+      mark: {
+        value: mark
+      }, 
+      
+    })
+};
+// hard code the names and marks for now
+const player1 = Players("playerOne", "O");
+const player2 = Players("playerTwo", "X");
+//using an IIFE module because gameBoard is only needed once
+//I want to utilize the methods inside of it and use it's privacy
+const GameBoard = (function () {
+  var box;
+  const boxCont = document.querySelector(".item-cont");
+  const createBox = ()=> {
+
+    for (let i=0; i < 9; i++) {
+        box = document.createElement("div");
+        box.setAttribute("class", `item ${i}`);
+        boxArray.push(box);
+        boxCont.appendChild(box);       
+    } 
+  }
+  // put the logic in this function
+  const sayWinner = () => {
+    console.log(boxArray[1].innerText)
+  }
+  return {
+    sayWinner,
+    createBox, 
+  };
+}());
+GameBoard.createBox();
+
+// use currentPlayer = player1 to make turns for your players
+let currentPlayer = player1;
+function runGame() {
+  boxArray.forEach(item=>{
+    item.addEventListener("click", game)
+  })
+}
+runGame();
+function game (e) {
+  if (currentPlayer == player1) {
+    currentPlayer = player2;
+    if (e.target.textContent == "") {
+      e.target.textContent = player1.mark;
+      GameBoard.sayWinner();
+    };
     
-    return {
-      name,
-      mark,
-    }
+  } else if (currentPlayer == player2) {
+    currentPlayer = player1;
+    if (e.target.textContent == "") {
+      e.target.textContent = player2.mark;
+      GameBoard.sayWinner();
+    };
+  };
 };
-// make the mark for computer O, until later.
-const player1 = Players("computer", "O");
-const player2 = Players("user", "X");
-console.log(player1, player2)
-// control the flow of the game.
 
-const displayController = () => {
-
-};
